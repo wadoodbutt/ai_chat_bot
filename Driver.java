e06f0f9aa8f986627be36e5d228b95d2370a4655
wadoodbutt <109189695+wadoodbutt@users.noreply.github.com>
2022-07-14 13:13:24 -0400

    Create Driver.java

---
+++ Driver.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+/* The Driver class simply runs the program, utilizing the GUI class primarily.
+ */
+public class Driver {
+    public static void main(String[] args) throws IOException {
+        GUI gui = new GUI();
+    }
+}
